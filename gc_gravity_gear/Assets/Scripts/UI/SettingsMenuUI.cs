ae22247869287ece65abefe498aee0a905f2c787
Laura Huovinen <laura.huovinen@outlook.com>
2021-06-22 16:01:19 +0300

    Base for main menu and settings screen
    - Also made own folder for drone code

---
+++ gc_gravity_gear/Assets/Scripts/UI/SettingsMenuUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenuUI : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void BackToMainMenu()
+    {
+        Debug.Log("Back pressed, go to main menu");
+        SceneManager.LoadScene(0);
+    }
+}
